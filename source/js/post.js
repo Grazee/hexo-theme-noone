90eda1cb5994142c9cd8695c74715fbe5565e674
喂草。 <lkw91110@gmail.com>
2021-03-05 12:20:35 +0800

    文章图片点击全屏。

--- source/js/post.js
+++ source/js/post.js
@@ -1,11 +1,35 @@
+// 删除图片后面的换行符
 $("article#post img")
     .next()
     .remove();
 
+// 添加图片标题
 $("article#post img")
     .each(function() {
         if ($(this).attr("alt")) {
             var title = $(this).attr("alt");
             $(this).after("<span class=\"image-title\">" + title + "</span>");
         }
+    });
+
+// 点击放大图片
+$("article#post img")
+    .click(function() {
+        showImage($(this));
+    });
+
+function showImage(image) {
+    var div = $("div#fullscreen-image");
+    var img = $("div#fullscreen-image img");    
+    div.show();
+    img.attr("src", image.attr("src"));
+    var top = (div.height() - img.height()) / 2;
+    var left = (div.width() - img.width()) / 2;
+    img.css("top", top);
+    img.css("left", left);
+}
+
+// 点击方法的图片关闭
+$("div#fullscreen-image").click(function(){
+    $("div#fullscreen-image").hide();
 });
